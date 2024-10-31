57f3c8639b280ab553e177620425558f1f06a878
cmbayne10 <cmbayne10@gmail.com>
2024-10-31 07:57:17 -0400

    ITS ALIVE!!!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MyService } from './my-service.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,19 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'routing-app';
+
+  items: any[] = [];
+
+  constructor(private myService: MyService) {
+      this.items = this.myService.getData();
+  }
+
+  addItem() {
+      const newItem = { id: Date.now(), value: 'New Item' };
+      this.myService.addData(newItem);
+      this.items = this.myService.getData();
+  }
 }
+
+
+
---
+++ src/app/my-service.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MyService {
+  private data: any[] = [];
+
+  getData() {
+    return this.data;
+  }
+
+  addData(item: any) {
+    this.data.push(item);
+  }
+}
+
+export class MyServiceService {
+  private data: any[] = [];
+
+  getData() {
+    return this.data;
+  }
+
+  addData(item: any) {
+    this.data.push(item);
+  }
+}
