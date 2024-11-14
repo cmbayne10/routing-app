71245dbd15f3dfe13a7078b62466b4e3b52d4ec3
cmbayne10 <cmbayne10@gmail.com>
2024-11-14 08:08:17 -0500

    Organized links/fetches data

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-first',
   standalone: true,
   imports: [],
   templateUrl: './first.component.html',
-  styleUrl: './first.component.css'
+  styleUrl: './first.component.css',
 })
 export class FirstComponent {
+  constructor(private http: HttpClient) {}
 
+  @Input() data= 'Результаты будут опубликованы здесь'
+
+   async buttonPress() {
+    const response = this.http.get<any>('https://pokeapi.co/api/v2/pokemon/ditto')
+    const result = await firstValueFrom(response)
+    console.log({data: result});
+    this.data = JSON.stringify(result)
+  }
 }
